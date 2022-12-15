223e474a1b73a36ac73619f844726b64feaaf730
Aone <aoneshrestha58@gmail.com>
2022-12-14 21:29:16 -0600

    add new task

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-
-import "./App.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faCircleCheck,
   faPen,
   faTrashCan,
 } from "@fortawesome/free-solid-svg-icons";
+
+import "./App.css";
 
 function App() {
   const [toDo, setToDo] = useState([
@@ -17,7 +17,14 @@ function App() {
   ]);
   const [newTask, setNewTask] = useState("");
   const [updateData, setUpdateData] = useState("");
-  const addTask = () => {};
+  const addTask = () => {
+    if (newTask) {
+      let num = toDo.length + 1;
+      let newEntry = { id: num, title: newTask, status: false };
+      setToDo([...toDo, newEntry]);
+      setNewTask("");
+    }
+  };
   const deleteTast = (id) => {};
   const markDone = (id) => {};
   const cancelUpdate = () => {};
@@ -25,37 +32,76 @@ function App() {
   const updateTast = () => {};
 
   return (
-    <div className="App">
-      <br />
-      <h2>To Do List</h2>
-      <br />
+    <div className="container App ">
+      <div class="row d-flex justify-content-center align-items-center">
+        <div class="col-12 col-md-9 col-lg-7 col-xl-6">
+          <br />
+          <h2>To Do List</h2>
+          <br />
+          <div className="row">
+            <div className="col">
+              <input
+                value={newTask}
+                onChange={(e) => {
+                  setNewTask(e.target.value);
+                }}
+                className="form-control"
+              />
+            </div>
+            <div className="col-auto">
+              <button
+                type="submit"
+                onClick={addTask}
+                className="btn btn-success"
+              >
+                Add Task
+              </button>
+            </div>
+          </div>
+          <br />
 
-      {toDo && toDo.length ? "" : "No Tasks"}
-      {toDo &&toDo
-      .sort((a,b) => a.id - b.id )
-      .map((task, index) => {
-          return (
-            <React.Fragment key={task.id}>
-              <div className="col taskBg">
-                <div className={task.status ? "done" : ""}>
-                  <span className="taskNumber">{task.id} </span>
-                  <span className="taskText">{task.title}</span>
-                </div>
-                <div className="iconsWrap">
-                  <span>
-                    <FontAwesomeIcon icon={faCircleCheck} />
-                  </span>
-                  <span>
-                    <FontAwesomeIcon icon={faPen} />
-                  </span>
-                  <span>
-                    <FontAwesomeIcon icon={faTrashCan} />
-                  </span>
-                </div>
-              </div>
-            </React.Fragment>
-          );
-        })}
+          <div className="row">
+            <div className="col">
+              <input className="form-control " />
+            </div>
+            <div className="col-auto">
+              <button className="btn btn-success">Update</button>
+              <button className="btn btn-warning m-1">Cancel</button>
+            </div>
+          </div>
+          <br />
+
+          {toDo && toDo.length ? "" : "No Tasks"}
+          {toDo &&
+            toDo
+              .sort((a, b) => a.id - b.id)
+              .map((task, index) => {
+                return (
+                  <React.Fragment key={task.id}>
+                    <div className="col taskBg">
+                      <div className={task.status ? "done" : ""}>
+                        <span className="taskNumber">{task.id} </span>
+                        <span className="taskText">{task.title}</span>
+                      </div>
+                      <div className="iconsWrap">
+                        <span>
+                          <FontAwesomeIcon icon={faCircleCheck}
+                          
+                          />
+                        </span>
+                        <span>
+                          <FontAwesomeIcon icon={faPen} />
+                        </span>
+                        <span>
+                          <FontAwesomeIcon icon={faTrashCan} />
+                        </span>
+                      </div>
+                    </div>
+                  </React.Fragment>
+                );
+              })}
+        </div>
+      </div>
     </div>
   );
 }
