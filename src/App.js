eb56edbd04b821e7f711f3545e9d7818e49c0267
Aone <aoneshrestha58@gmail.com>
2022-12-14 18:01:49 -0600

    initialize project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import "./App.css";
 
 function App() {
+
+  const[toDo,setToDo] = useState([
+    {"id": 1, "title":"Task 1", "status": false},
+    {"id": 2, "title":"Task 2", "status": false},
+
+    
+  ])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <br />
+      <h2>To Do List</h2>
     </div>
   );
 }
